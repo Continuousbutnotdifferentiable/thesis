b6e29af8476293d95ee06a8591654b50d92ee9c2
mike_kalange <continuousbutnotdifferentiable@gmail.com>
2018-10-04 17:51:09 -0700

    begins implementing stopword model trainer, adds argv for stopword

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # thesis
 
 working repo for thesis
+
+apparently gensim already downsamples stopwords when its doing the model training.
+what this means for me tho is that the distance implementation still needs to strip out stopwords
+
+todo:
+implement stopword stripping
+build stopword free keyed vectors
+plot stopword free graphs against stopword "full" graphs
--- my_little_python_word2vec.py
+++ my_little_python_word2vec.py
@@ -11,9 +11,12 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 # Sysarg must be a .txt.gz
 inFile  = sys.argv[1]
-#outWordVecs = sys.argv[2]
-#outFile = sys.argv[3]
-
+nostop = sys.argv[2]
+50string = "vecs_50"
+100string = "vecs_100"
+150string = "vecs_150"
+200string = "vecs_200"
+nostopstring = "_nostop"
 
 def read_input(input_file):
     """This method reads the input file which is in gzip format"""
@@ -34,21 +37,26 @@ def read_input(input_file):
 documents = list (read_input (inFile))
 logging.info ("Done reading data file")
 
+# Trains 4 models using vector sizes 50, 100, 150, and 200 
 model100 = gensim.models.Word2Vec(documents,size=100,window=10,min_count=2,workers=10)
 model100.train(documents, total_examples=len(documents), epochs=10)
+word_vectors_100 = model100.wv
 
 model150 = gensim.models.Word2Vec(documents,size=150,window=10,min_count=2,workers=10)
 model150.train(documents, total_examples=len(documents), epochs=10)
+word_vectors_150 = model150.wv
 
 
 model200 = gensim.models.Word2Vec(documents,size=200,window=10,min_count=2,workers=10)
 model200.train(documents, total_examples=len(documents), epochs=10)
-
-word_vectors_100 = model100.wv
-word_vectors_100.save("reviews_vecs_100.kv")
-
-word_vectors_150 = model150.wv
-word_vectors_150.save("reviews_vecs_150.kv")
-
 word_vectors_200 = model200.wv
-word_vectors_200.save("reviews_vecs_200.kv")
+
+if nostop == "nostop";
+    word_vectors_100.save(100string+nostop+".kv")
+    word_vectors_150.save(150string+nostop+".kv")
+    word_vectors_200.save(200string+nostop+".kv")
+
+else:
+    word_vectors_100.save(100string+".kv")
+    word_vectors_150.save(150string+".kv")
+    word_vectors_200.save(200string+".kv")
