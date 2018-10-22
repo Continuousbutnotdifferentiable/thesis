81ed4ea8dbe7a769e9997f6e84519eab55b36113
mike_kalange <continuousbutnotdifferentiable@gmail.com>
2018-10-21 22:44:46 -0700

    done

--- reviews_vecs/distance_vector_compressor.py
+++ reviews_vecs/distance_vector_compressor.py
@@ -1,30 +1,20 @@
 import gensim
 import pickle
-<<<<<<< HEAD
 import csv
 import sys
 import gzip
 import logging
 from scipy.spatial import distance
-=======
-import sys
-import gzip
-import logging
->>>>>>> 37fb428456ad081afdfa5dae8149c827dc13b963
 from gensim.models import KeyedVectors
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-<<<<<<< HEAD
 def headToHead(word1,word2):
     newVec = []
     for i in range(0,len(word1)):
         newVec.append(word2[i]-word1[i])
     return newVec
 
-=======
-# List of ntlkStopwords current as of July 6, 2018
->>>>>>> 37fb428456ad081afdfa5dae8149c827dc13b963
 
 nostop = True
 inFile = sys.argv[2]
@@ -41,7 +31,6 @@ if nostop:
     word_vectors_70 = KeyedVectors.load("vecs_70_nostop.kv", mmap='r')
     word_vectors_75 = KeyedVectors.load("vecs_75_nostop.kv", mmap='r')
     
-<<<<<<< HEAD
 vectorDictionary = {"vecs_25_":word_vectors_25, "vecs_30_":word_vectors_30,"vecs_35_":word_vectors_35,
                     "vecs_40_":word_vectors_40, "vecs_45_":word_vectors_45,"vecs_50_":word_vectors_50,
                     "vecs_55_":word_vectors_55, "vecs_60_":word_vectors_60,"vecs_65_":word_vectors_65,
@@ -91,26 +80,3 @@ for string, vector in vectorDictionary.items():
                 for item in fileArray:
                     f.write("%s\n" % item)
             f.close()
-=======
-    vectorDictionary = {"vecs_25_":word_vectors_25, "vecs_30_":word_vectors_30,"vecs_35_":word_vectors_35,
-                        "vecs_40_":word_vectors_40, "vecs_45_":word_vectors_45,"vecs_50_":word_vectors_50,
-                        "vecs_55_":word_vectors_55, "vecs_60_":word_vectors_60,"vecs_65_":word_vectors_65,
-                        "vecs_70_":word_vectors_70, "vecs_75_":word_vectors_75}
-
-print("Vectors Loaded")
-
-fileObject = open(inFile,'r')  
-documents = pickle.load(fileObject)
-
-for string, vector in vectorDictionary:
-    theVectors = word_vectors
-    for file in range(0,99):
-        fileList = []
-        for i in range(0,len(documents[file])-1):
-            if documents[file][i] in word_vectors.vocab and documents[file][i+1] in word_vectors.vocab:
-                
-        distancesInternal10000.append(fileList)
-
-
-tipToTail(word1,word2):
->>>>>>> 37fb428456ad081afdfa5dae8149c827dc13b963
